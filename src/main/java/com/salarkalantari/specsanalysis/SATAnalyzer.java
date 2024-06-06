6119590b7cb2d15e4c5a8fd2fd54ab7f308df828
Salar Kalantari <salarkalantari@yahoo.com>
2024-06-06 17:20:37 +0200

    update SAT Analyzer

--- src/main/java/com/salarkalantari/specsanalysis/SATAnalyzer.java
+++ src/main/java/com/salarkalantari/specsanalysis/SATAnalyzer.java
@@ -1,5 +1,8 @@
 package com.salarkalantari.specsanalysis;
 
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 //import com.yourname.specsanalysis.model.SpecAnalysisResult;
 
 public class SATAnalyzer implements SpecAnalyzer {
@@ -7,10 +10,24 @@ public class SATAnalyzer implements SpecAnalyzer {
     public SpecAnalysisResult analyze(String filePath) {
         SpecAnalysisResult result = new SpecAnalysisResult();
         
-        // Add logic to run and analyze SAT specifications
-        // For example, run the SAT solver and capture the output
-        // Analyze the output to extract the required metrics
+        //TODO first read the spec form path and then set it to LOC
         
         return result;
+    }
+    
+    
+    
+    private static int getSATLOC(String spec) {
+        int lineCount = 0;
+        //TODO not sure about the \r\n
+        String[] lines = spec.split("\r\n"); // Split the code into lines
+
+        for (String line : lines) {
+            // Trim whitespace and check if the line is neither empty nor a comment
+            if (!line.trim().isEmpty() && !line.trim().startsWith("%")) {
+                lineCount++; // Increment count for valid lines of code
+            }
+        }
+        return lineCount;
     }
 }
